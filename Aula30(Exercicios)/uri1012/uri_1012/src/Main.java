7892a60d28b4493de6f4ca4dccf4236ec4131944
AntonioAiltonGoncalves <antonioailton723@gmail.com>
2021-07-25 17:32:47 -0300

    uri 1012

---
+++ Aula30(Exercicios)/uri1012/uri_1012/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
+        Scanner in = new Scanner(System.in);
+        double A = in.nextDouble();
+        double B = in.nextDouble();
+        double C = in.nextDouble();
+        in.close();
+        
+        double pi = 3.14159;
+        
+        double triangulo = A*C/2;
+        double circulo = pi * (C*C);
+        double trapezio = ((A+B)*C)/2;
+        double quadrado = B*B;
+        double retangulo = A*B;
+        
+        System.out.printf("TRIANGULO: %.3f%n",triangulo);
+        System.out.printf("CIRCULO: %.3f%n",circulo);
+        System.out.printf("TRAPEZIO: %.3f%n",trapezio);
+        System.out.printf("QUADRADO: %.3f%n",quadrado);
+        System.out.printf("RETANGULO: %.3f%n",retangulo);
+ 
+    }
+}
